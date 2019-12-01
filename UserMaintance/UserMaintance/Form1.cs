4dd905c98ac64af76331e0606ba6942d7d41bfff
Péter Márk Toldik <peter.toldik@stud.uni-corvinus.hu>
2019-12-01 21:50:55 +0100

    commit8

--- UserMaintance/UserMaintance/Form1.cs
+++ UserMaintance/UserMaintance/Form1.cs
@@ -54,5 +54,13 @@ namespace UserMaintance
                 }
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                users.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
     }
 }
